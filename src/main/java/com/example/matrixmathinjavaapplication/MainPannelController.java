266b5fcac90f47e3805ae1c84bb02391a15294f5
MahiRavanKRG <94435007+MahiRavanKRG@users.noreply.github.com>
2022-09-01 23:52:57 +0530

    ...

--- src/main/java/com/example/matrixmathinjavaapplication/MainPannelController.java
+++ src/main/java/com/example/matrixmathinjavaapplication/MainPannelController.java
@@ -1,16 +1,13 @@
 /**
- *      This is not COMMENTED
+ *      created double . dont just do random bull hit on git.
  */
 
 package com.example.matrixmathinjavaapplication;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
-import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
 
 public class MainPannelController {
 
@@ -21,10 +18,7 @@ public class MainPannelController {
     private Label heading;
 
 
-//    private Stage primaryStage ;
-//    private Scene scene ;
-//    private Node root ;
-//    public double[][] xMatrix = new double[3][3] ;
+private double[][] controllerXMatrix = new double[3][3];
 
 
     public void OnClearButtonPress(ActionEvent event){
@@ -47,18 +41,12 @@ public class MainPannelController {
 
     public void OnCalculateButtonPress(ActionEvent event){
 
+
+
         heading.setText("Determinant is " + " ");
 
 
-        a11.setText("0");
-        a12.setText("0");
-        a13.setText("0");
-        a21.setText("0");
-        a22.setText("0");
-        a23.setText("0");
-        a31.setText("0");
-        a32.setText("0");
-        a33.setText("0");
+
 
 
 
