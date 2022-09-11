d79abebf04c8cd2c5f82ef33cf06d4c19d356b2f
MahiRavanKRG <94435007+MahiRavanKRG@users.noreply.github.com>
2022-09-11 14:08:31 +0530

    Tried and finished up to certain extent step 2 of ControllerUDOApplication.java

--- src/main/java/com/example/matrixmathinjavaapplication/ControllerUDOApplication.java
+++ src/main/java/com/example/matrixmathinjavaapplication/ControllerUDOApplication.java
@@ -5,8 +5,11 @@
  *
  *      I think of implementing this in steps,
  *          step 1: add extra requirements to the fxml file of InverseMatrixApplication. DONE
- *          step 2: test for one operation of multiplication, the chosen operation is row multiplication. working
- *          step 3: add the rest of the elements to the fxml file and add proper methods and finish.
+ *          step 2: test for one operation of multiplication, the chosen operation is row multiplication. DONE
+ *
+ *              > The Determinant button doesn't work as expected.
+ *
+ *          step 3: add the rest of the elements to the fxml file and add proper methods and finish. working
  */
 
 package com.example.matrixmathinjavaapplication;
@@ -23,7 +26,8 @@ public class ControllerUDOApplication {
 
     @FXML
     private TextField a11 , a12 , a13 , a21 , a22 , a23 , a31 , a32 , a33;
-    private TextField inputRow , inputVar;
+    @FXML
+    private TextField inputRow1 , inputRow2 , inputVar;
 
     @FXML
     private Button Calculate , Determinant;
@@ -37,24 +41,64 @@ public class ControllerUDOApplication {
             };
 
             var inputMulVar = Double.parseDouble(inputVar.getText());
-            var inputRowVar = Integer.parseInt(inputRow.getText());
+            var inputRowVar = Integer.parseInt(inputRow1.getText());
 
             Matrix controllerUDOMatrixObject = new Matrix(controllerUDOMatrix);
 
-            controllerUDOMatrixObject.rowMultiplication( controllerUDOMatrixObject.matrix[0] , inputMulVar );
+            Exception exception = new Exception();
 
-            a11.setText(String.valueOf(controllerUDOMatrixObject.matrix[0][0]));
-            a12.setText(String.valueOf(controllerUDOMatrixObject.matrix[0][1]));
-            a13.setText(String.valueOf(controllerUDOMatrixObject.matrix[0][2]));
+            DeterminantPrompt.setText(".");
+
+            if (-1<inputRowVar && inputRowVar<4){
+                int targetRow = inputRowVar - 1;
+                controllerUDOMatrixObject.rowMultiplication(controllerUDOMatrixObject.matrix[targetRow], inputMulVar);
+
+               if (targetRow==0){
+                    a11.setText(String.valueOf(controllerUDOMatrixObject.matrix[targetRow][0]));
+                    a12.setText(String.valueOf(controllerUDOMatrixObject.matrix[targetRow][1]));
+                    a13.setText(String.valueOf(controllerUDOMatrixObject.matrix[targetRow][2]));
+                }
+               else if (targetRow==1) {
+                   a21.setText(String.valueOf(controllerUDOMatrixObject.matrix[targetRow][0]));
+                   a22.setText(String.valueOf(controllerUDOMatrixObject.matrix[targetRow][1]));
+                   a23.setText(String.valueOf(controllerUDOMatrixObject.matrix[targetRow][2]));
+               }
+               else if (targetRow==2) {
+                   a31.setText(String.valueOf(controllerUDOMatrixObject.matrix[targetRow][0]));
+                   a32.setText(String.valueOf(controllerUDOMatrixObject.matrix[targetRow][1]));
+                   a33.setText(String.valueOf(controllerUDOMatrixObject.matrix[targetRow][2]));
+               }
+            }
+           else
+               DeterminantPrompt.setText("Enter Valid Row Value");
 
         } catch (Exception e) {
-            e.printStackTrace();
+           // e.printStackTrace();
+            DeterminantPrompt.setText("Enter all the values in the text box");
         }
 
     }
 
 
     public void OnDeterminantButtonPress(){
+        try {
+            double[][] controllerUDOMatrix = new double[][]{
+                    {Double.parseDouble(a11.getText()), Double.parseDouble(a12.getText()), Double.parseDouble(a13.getText())},
+                    {Double.parseDouble(a21.getText()), Double.parseDouble(a22.getText()), Double.parseDouble(a23.getText())},
+                    {Double.parseDouble(a31.getText()), Double.parseDouble(a32.getText()), Double.parseDouble(a33.getText())}
+            };
 
+            Matrix controllerUDOMatrixObject = new Matrix(controllerUDOMatrix);
+
+            DeterminantPrompt.setText( "The determinant of the given matrix is " + controllerUDOMatrixObject.determinantOfMatrix);
+
+        }catch (Exception exception){
+            exception.printStackTrace();
+        }
+
+    }
+
+    public void OninputRow1Leave(){
+        inputRow2.setText(inputRow1.getText());
     }
 }
